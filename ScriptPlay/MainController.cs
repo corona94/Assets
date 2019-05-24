310eaee5f8ab390c71d88225938e18ed30864b1b
corona94 <51008000+corona94@users.noreply.github.com>
2019-05-24 18:21:25 -0500

    Initial commit

---
+++ ScriptPlay/MainController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class MainController : MonoBehaviour
+{
+   
+   //string name = "PlayGame";
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.Joystick1Button0))
+        {
+            SceneManager.LoadScene("playgame");
+        }
+        else if(Input.GetKey(KeyCode.Joystick1Button1))
+            {
+            Application.Quit();
+        }
+
+    }
+
+    /*public void SceneChange(string name)
+    {
+        SceneManager.LoadScene(name);
+    }*/
+   
+}
